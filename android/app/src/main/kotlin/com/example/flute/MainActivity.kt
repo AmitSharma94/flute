077234e3f75023670ca3182e6ad97ec4d9d29f68
AmitSharma94 <amitks9494@gmail.com>
2026-07-15 20:18:28 +0530

    fix services

--- android/app/src/main/kotlin/com/example/flute/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flute/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.flute
 
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : AudioServiceActivity()
