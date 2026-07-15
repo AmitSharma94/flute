0ae548469c3adf2d5ea276c2d9bba1c634a63794
AmitSharma94 <amitks9494@gmail.com>
2026-07-15 20:44:07 +0530

    tempbuildfix

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:audio_service/audio_service.dart';
 
 import 'app/app.dart';
-import 'core/audio/audio_handler.dart';
-import 'core/audio/providers/audio_handler_provider.dart';
 
-late FluteAudioHandler audioHandler;
-
-Future<void> main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  audioHandler = await AudioService.init(
-    builder: () => FluteAudioHandler(),
-    config: const AudioServiceConfig(
-      androidNotificationChannelId: 'com.flute.audio',
-      androidNotificationChannelName: 'Flute Music',
-      androidNotificationOngoing: true,
-    ),
-  );
-
   runApp(
-    ProviderScope(
-      overrides: [
-        audioHandlerProvider.overrideWithValue(
-          audioHandler,
-        ),
-      ],
-      child: const FluteApp(),
+    const ProviderScope(
+      child: FluteApp(),
     ),
   );
 }
