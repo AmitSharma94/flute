5de836033f58792a8eccf712bd8e36a5fe04d7ce
AmitSharma94 <amitks9494@gmail.com>
2026-07-18 01:11:16 +0530

    Align on_audio_query Java and Kotlin JVM targets

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -13,21 +13,15 @@ val newBuildDir: Directory =
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
-subprojects {
     if (name == "on_audio_query_android") {
         pluginManager.withPlugin("com.android.library") {
-            extensions.configure<LibraryExtension> {
+            extensions.configure<com.android.build.gradle.LibraryExtension> {
                 namespace = "com.lucasjosino.on_audio_query"
+
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
             }
         }
     }
