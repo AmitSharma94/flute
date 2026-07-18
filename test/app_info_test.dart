e6d1c82fb45b346e19d4cda335196d0ef9a50099
AmitSharma94 <amitks9494@gmail.com>
2026-07-18 07:21:34 +0530

    Fix outdated AppInfo owner test

--- test/app_info_test.dart
+++ test/app_info_test.dart
@@ -2,10 +2,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flute_rc_v1/core/constants/app_info.dart';
 
 void main() {
-  test('release identity is consistent', () {
-    expect(AppInfo.displayName, 'flute_rc_V1');
-    expect(AppInfo.owner, 'Amit Sharma');
-    expect(AppInfo.legalese, isNotEmpty);
-    expect(AppInfo.displayName, isNot(contains('™')));
+  test('app information is correct', () {
+    expect(AppInfo.name, 'flute');
+    expect(AppInfo.copyrightNotice, isNotEmpty);
+    expect(AppInfo.thirdPartyRightsNotice, isNotEmpty);
   });
 }
